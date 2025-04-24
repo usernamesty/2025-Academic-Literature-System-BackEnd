43f5bbea4113bb5d9f45270cc833560a92ca8416
StevenMaoX <22373184@buaa.edu.cn>
2025-04-24 20:12:48 +0800

    settings恢复初始设置

--- Epp-BackEnd/backend/backend/settings.py
+++ Epp-BackEnd/backend/backend/settings.py
@@ -120,12 +120,8 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'academicsys',
-        'USER': 'root',
-        'PASSWORD': 'Maoenqi130450',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
